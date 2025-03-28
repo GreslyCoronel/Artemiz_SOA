844318c1a8a4a758e8c32fa57d78b4516aaed62b
GreslyCoronel <gdcoronelo@ufpso.edu.co>
2025-03-27 23:17:03 -0500

    Login y registro

--- Artemiz/src/app/app.routes.ts
+++ Artemiz/src/app/app.routes.ts
@@ -1,3 +1,17 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { RegistroComponent } from './registro/registro.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: 'login',
+        title: 'login',
+        component: LoginComponent
+    },
+    {
+        path:'registro',
+        title:'registro',
+        component: RegistroComponent
+    }
+
+];
